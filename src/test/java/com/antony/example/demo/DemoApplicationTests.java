7d7efc372efc341d87993bffa9664df1c8d96d1d
antony <pared33.av@gmail.com>
2021-10-06 14:31:08 -0500

    initial commint

---
+++ src/test/java/com/antony/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antony.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
